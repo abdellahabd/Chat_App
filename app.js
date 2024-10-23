bfa5bec915b968d6f65227bc67a992449e7ab4cf
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-10-23 10:40:02 +0100

    add styles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import path from "path";
 import { fileURLToPath } from "url";
+import { Server } from "socket.io";
 
 const app = express();
 const port = process.env.port || 3500;
@@ -11,5 +12,13 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
 const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id.substring(0, 5));
+});
 
 httpServer.listen(port);
