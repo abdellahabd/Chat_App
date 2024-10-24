4c12046684e003432388d0fbed16aacedabdd8d3
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-10-24 09:34:44 +0100

    add messages to Ui

--- public/main.js
+++ public/main.js
@@ -4,6 +4,7 @@ const client_total = document.querySelector(".clients-total");
 const message_form = document.getElementById("message-form");
 const message_input = document.getElementById("message-input");
 const name_input = document.getElementById("name-input");
+const message_contiener = document.getElementById("message-container");
 
 message_form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,10 +17,23 @@ function sendMessage() {
     message: message_input.value,
     date: new Date(),
   };
+  socket.emit("message", data);
+
+  addMessageToUi(true, data);
 
   message_input.value = "";
+}
 
-  socket.emit("message", data);
+function addMessageToUi(isfromMe, data) {
+  const element = `     
+   <li class=${isfromMe ? "message-right" : "message-left"}>
+          <p class="message">
+        ${data.message}
+            <span>${data.name} ● ${moment(data.date).fromNow()} </span>
+          </p>
+        </li>`;
+
+  message_contiener.innerHTML += element;
 }
 
 socket.on("Client-total", (data) => {
@@ -27,5 +41,5 @@ socket.on("Client-total", (data) => {
 });
 
 socket.on("chat_message", (data) => {
-  console.log(data);
+  addMessageToUi(false, data);
 });
